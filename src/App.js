57717638e95b334bbe48320df6efb33a099be543
Mehejabin <RaihanFaruk>
2022-04-17 22:12:54 +0600

    parina tarporeo cesta

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/home'
 import Login from './components/Login/login';
 import Header from './components/header/header';
+import Service from './components/service/service';
+import Blog from './components/blog/blog'
+import About from './components/about/about';
+import RequireAuth from './components/blog/requireauth/requireAuth';
 function App() {
   return (
     <div className="App">
@@ -10,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
+        {/* <Route path="/service" element={<RequireAuth><Service></Service></RequireAuth>}></Route> */}
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
       </Routes>
     </div>
   );
